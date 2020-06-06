007c4688247fa0c41e6e1136266e0d1c23f7ee56
SubhoBasak <subhobasak50@gmail.com>
2020-06-06 10:24:22 +0530

    Add splash screen and creat histogram with threading with the exit bug.

--- run.py
+++ run.py
@@ -1,15 +1,18 @@
 import os
 import sys
+import requests
+import threading
 import webbrowser
 from datetime import datetime
 
-from PyQt5.QtWidgets import QMainWindow, QApplication, QFileDialog, QMessageBox, QWidget
-from PyQt5.QtCore import QAbstractTableModel, Qt
-from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QMainWindow, QApplication, QFileDialog, QMessageBox, QWidget, QSplashScreen
+from PyQt5.QtCore import QAbstractTableModel, Qt, QTimer
+from PyQt5.QtGui import QIcon, QPixmap
 from main_window import Ui_MainWindow
 from about_window import Ui_About
 
 import pandas as pd
+import matplotlib.pyplot as plt
 
 class PandasModel(QAbstractTableModel):
     def __init__(self, data):
@@ -35,8 +38,10 @@ class PandasModel(QAbstractTableModel):
 
 class Setup():
     def __init__(self):
+        self.loading_screen()
         self.data = None
         self.file = None
+        self.version = '1.0.0'
 
         self.root = QMainWindow()
         self.window = Ui_MainWindow()
@@ -48,9 +53,10 @@ class Setup():
         self.window.menuFile.addAction(QIcon('icons/save.ico'), 'Save', self.save_func, 'Ctrl+S')
         self.window.menuFile.addAction(QIcon('icons/save_as.ico'), 'Save As', self.save_as_func, 'Ctrl+Shift+S')
         self.window.menuFile.addSeparator()
-        self.window.menuFile.addAction(QIcon('icons/exit.ico'), 'Quit', lambda : 0, 'Alt+F4') # todo, make this ready for deploy
+        self.window.menuFile.addAction(QIcon('icons/exit.ico'), 'Quit', lambda: 0,
+                                       'Alt+F4')  # todo, make this ready for deploy
 
-        self.window.menuView.addAction(QIcon('icons/preview.ico'), 'Preview', self.open_func, 'Ctrl+P')
+        self.window.menuView.addAction(QIcon('icons/preview.ico'), 'Preview', self.create_hist, 'Ctrl+P')
 
         self.window.menuHelp.addAction(QIcon('icons/about.ico'), 'About', self.about_func, 'Ctrl+H')
         self.window.menuHelp.addAction(QIcon('icons/update.ico'), 'Check for update', self.open_func, 'Ctrl+U')
@@ -60,8 +66,14 @@ class Setup():
         self.window.select_button.clicked.connect(self.select_func)
         self.window.copy_csv_button.clicked.connect(self.copy_func)
         self.window.save_button.clicked.connect(self.save_func)
+        self.window.preview_button.clicked.connect(self.create_hist)
 
         self.root.show()
+
+    def loading_screen(self):
+        self.splash_screen = QSplashScreen(QPixmap('icons/logo.png'))
+        self.splash_screen.show()
+        QTimer.singleShot(1000, self.splash_screen.close)
 
     def open_func(self):
         self.file_dialog = QFileDialog()
@@ -100,13 +112,13 @@ class Setup():
             self.msg_box.setInformativeText('New csv file is saved as '+tmp)
             self.msg_box.show()
 
-    def save_func(self):
+    def save_func(self): # todo, compelete this function
         if self.file:
             print(self.file)
         else:
             self.save_as_func(title = 'Save')
 
-    def save_as_func(self, title = 'Save As'):
+    def save_as_func(self, title = 'Save As'): # todo, complete this function
         self.file, _ = QFileDialog.getSaveFileName(caption=title)
 
     def about_func(self):
@@ -119,6 +131,26 @@ class Setup():
         self.about.doc_button.clicked.connect(lambda : webbrowser.open(tmp))
 
         self.about_widget.show()
+
+    def check_update(self): # todo, complete the function
+        resp = requests.get(url = 'https://www.google.com') # todo, change to the correct url
+        if resp.json().get('version') != self.version:
+            pass
+
+    def create_hist(self):
+        self.thread = threading.Thread(target = self.__create_hist)
+        self.thread.start()
+
+    def __create_hist(self):
+        if self.data.__class__.__name__ == 'DataFrame':
+            for file in os.listdir('Tmp'):
+                os.remove('Tmp/'+file)
+            fig = plt.figure(figsize=(12, 7))
+            for i, col in enumerate(self.data):
+                hist_data = self.data[col].value_counts().to_dict()
+                fig.clf()
+                plt.bar(hist_data.keys(), hist_data.values())
+                fig.savefig('Tmp/{}.png'.format(i))
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
